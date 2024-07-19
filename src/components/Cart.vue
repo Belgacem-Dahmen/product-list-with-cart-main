879df9e12e79cb86165d03322cb0fcf4ccab9774
Belgacem-Dahmen <belgacem.dahmen@gmail.comn>
2024-07-19 13:03:56 +0200

    updating cart logic

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="cart">
-        <h3> Your Cart ({{ numberOfArticles }})</h3>
+        <h3 class="cart__title"> Your Cart ({{ numberOfArticles }})</h3>
         <div v-if="isEmptyCart" class="cart-empty">
             <img src="../assets/images/illustration-empty-cart.svg" alt="">
             <p>your added items will appear here</p>
@@ -11,7 +11,7 @@
 
                     <div class="cart-item__infos">
                         <p class="cart-item__name">{{ article.name }}</p>
-                        <p> <span>{{ article.quantity }}x</span> <span>@ ${{ article.price }}</span> <span>${{
+                        <p> <span class="cart-item__quantity">{{ article.quantity }}x</span> <span>@ ${{ article.price }}</span> <span>${{
                             article.quantity *
                             article.price }}</span></p>
                     </div>
@@ -21,7 +21,17 @@
 
                 </div>
             </div>
-            {{ totalPrice }}
+            <div class="order-total">
+                <p class="order-total__text">Order Total </p>
+                <p class="order-total__value"> ${{ totalPrice }}</p>
+            </div>
+            <div class="order__delivery">
+                <img class="order__delivery-icon" :src="neutralIcon" alt="">
+                <p class="order__delivery-text">This is a <span class="bold">carbon-neutral</span> delivery</p>
+            </div>
+            <button class="order__delivery-confirmBtn "  >
+                Confirm Order
+            </button>
         </div>
     </div>
 </template>
@@ -29,7 +39,7 @@
 <script setup>
 import { computed } from 'vue';
 import iconRemove from "@/assets/images/icon-remove-item.svg"
-
+import neutralIcon from "@/assets/images/icon-carbon-neutral.svg"
 
 const articles = computed(() => {
     const uniqueArticlesMap = new Map();
@@ -90,6 +100,13 @@ const { cartData } = defineProps({
     justify-content: center;
     height: min-content;
 }
+
+.cart__title {
+    font-size: 1.5rem;
+    color: rgb(216, 63, 7);
+
+}
+
 .cart-items {
     display: flex;
     margin-top: 25px;
@@ -97,18 +114,62 @@ const { cartData } = defineProps({
     gap: 15px;
 
 }
+
 .cart-item {
     display: flex;
     justify-content: space-between;
     align-items: center;
+    border-bottom:  0.1px solid var(--rose-100);
+    padding-bottom: 10px;
+}
+.cart-item__name{
+    font-weight: var(--weight-bold);
+    font-size: 14px;
+    margin-bottom: 10px;
+}
+.cart-item__remove-btn {
+    border: 1px solid gray;
+    background: transparent;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    padding: 2px;
+
+
+}
+.cart-item__quantity {
+    color: rgb(216, 63, 7);
+    font-weight: var(--weight-bold);
+    
+}
+.order-total {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 25px;
+    border-radius: 10px;
 }
 
-.cart-item__remove-btn {
-   border: 1px solid gray;
-   background: transparent;
-    border-radius: 50%;
-    display: flex;align-items: center;padding: 2px;
-    
-    
+.order-total__value {
+    font-size: 2rem;
+    font-weight: var(--weight-bold);
+}
+
+
+.order__delivery {
+    padding: 20px 15px;
+    display: flex;
+    font-size: 14px;
+    gap: 10px;
+    background: var(--rose-100);
+}
+
+.order__delivery-confirmBtn {
+    background-color: rgb(216, 63, 7);
+    border-radius: 45px;
+    border: none;
+    color: var(--rose-100);
+    padding: 15px;
+    font-weight: var(--weight-semi-bold);
 }
 </style>
