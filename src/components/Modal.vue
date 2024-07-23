77340146379b28b84c7710dcb0353dde910cf3a4
Belgacem-Dahmen <belgacem.dahmen@gmail.comn>
2024-07-23 15:29:32 +0200

    adding styules to modal items

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -5,10 +5,10 @@
             <h2 class="modal__title">Order Confirmed</h2>
             <p class="modal__text">We hope you enjoy your food</p>
 
-            <div class="modal__items" v-for="article in articles" :key="article.id">
-                <div class="modal__item">
+            <div class="modal__items">
+                <div class="modal__item" v-for="article in articles" :key="article.id">
                     <div class="modal__item-infos">
-                        <div>
+                        <div class="modal__item-infos-left">
                             <img class="modal__item-img" :src="getImageUrl(article.image.thumbnail)" alt="">
                             <div>
                                 <p class="modal__item-name">{{ article.name }}</p>
@@ -19,12 +19,17 @@
                                 </p>
                             </div>
                         </div>
-                        <div>
 
-                            <span>${{ article.quantity * article.price }}</span>
+                        <div>
+                            <p><span>${{ article.quantity * article.price }}</span></p>
+
                         </div>
                     </div>
 
+                </div>
+                <div class="modal__order-infos">
+                    <p>Order total</p>
+                    <p> 125$</p>
                 </div>
             </div>
             <button id="close-button" @click="handleClose">Close</button>
@@ -82,7 +87,7 @@ const articles = computed(() => {
 
 .modal {
     background-color: white;
-    padding: 20px;
+    padding: 40px;
     border-radius: 8px;
     box-shadow: 0 2px 10px rgba(0, 0, 0, 0.9);
     text-align: left;
@@ -95,10 +100,41 @@ const articles = computed(() => {
 .modal__items {
     background: var(--rose-100);
     padding: 20px;
+    border-radius: 10px;
+    display: flex;
+    gap: 20px;
+    flex-direction: column;
+
 }
 
 .modal__item {
     display: flex;
+    border-radius: 10px;
+    gap: 20px;
+}
+
+.modal__item-img {
+    border-radius: 10px;
+    max-width: 50px;
+}
+
+.modal__item-infos {
+    display: flex;
+    min-width: 100%;
+    justify-content: space-between;
+
+}
+
+.modal__item-infos-left {
+    display: flex;
+    gap: 20px;
+}
+
+.modal__order-infos {
+    display: flex;
+    min-width: 100%;
+    justify-content: space-between;
+    align-items: center;
 }
 
 button {
